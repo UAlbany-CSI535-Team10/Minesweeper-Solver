9198d655472dc384b265adc8f71fd6c0886b4743
Santhosh Ranganathan <sranganathan2@albany.edu>
2019-11-05 20:48:58 -0500

    Add very dumb solver

--- game-logic/minesweeper.py
+++ game-logic/minesweeper.py
@@ -1,4 +1,6 @@
 from random import randint
+import numpy as np
+from queue import PriorityQueue
 
 def surrounding_cells(x, y, rows, cols):
   x_0, x_1 = x-1, x+2
@@ -35,13 +37,13 @@ def create_board(rows, cols, n):
 
 def display_state(state):
   rows, cols = len(state[0]), len(state[0][0])
-  rows_spacing, cols_spacing = len(str(rows)) + 1, len(str(cols)) + 1
+  rows_spacing, cols_spacing = len(str(rows)) + 1 +6, len(str(cols)) + 1 +6
   print('{:>{rs}}'.format(' ', rs=rows_spacing), end='')
   for i in range(cols):
-    print('{:>{cs}}'.format(i, cs=cols_spacing), end='')
+    print('{:>{cs}}'.format(i+1, cs=cols_spacing), end='')
   print('')
   for i in range(rows):
-    print('{:>{rs}}'.format(i, rs=rows_spacing), end='')
+    print('{:>{rs}}'.format(i+1, rs=rows_spacing), end='')
     for j in range(cols):
       print('{:>{cs}}'.format(state[0][i][j], cs = cols_spacing), end='')
     print('')
@@ -56,10 +58,10 @@ def play(row, col, state, board, flag=False):
     state[0][x][y] = 'X' if not flag else 'F'
     if flag:
       state[2] += 1
-    return not flag # game over only if mine clicked not flagged
-  if flag:
-    print('No mine here!')
-    return False
+    return True #not flag # game over only if mine clicked not flagged
+  # if flag:
+  #   print('No mine here!')
+  #   return False
   queue = [[x, y]]
   while queue:
     x, y = queue.pop(0)
@@ -67,13 +69,44 @@ def play(row, col, state, board, flag=False):
       continue
     state[1] += 1
     if board[x][y] > 0: # clicked on numbered cell
-      state[0][x][y] = board[x][y]
+      state[0][x][y] = str(board[x][y])
       continue
     # cell has no mine surrounding it 
     state[0][x][y] = ''
     # click on surrounding 8 cells
     queue += surrounding_cells(x, y, rows, cols)
   return False
+
+def ai_player(state, probabilties, explored, first=True):
+  rows, cols = len(state[0]), len(state[0][0])
+  if first:
+    return randint(0, rows-1), randint(0, cols-1)
+  cell_prob = PriorityQueue()
+  cellp = []
+  for i in range(rows):
+    for j in range(cols):
+      if state[0][i][j] == '':
+        explored[i][j] = 1
+        probabilties[i][j] = -100.0
+        for k, l in surrounding_cells(i, j, rows, cols):
+          probabilties[k][l] = -100.0
+      elif state[0][i][j].isdigit():
+        n = int(state[0][i][j])
+        if explored[i][j] != 0:
+          continue
+        explored[i][j] = 1
+        cells = [[k, l] for k, l in surrounding_cells(i, j, rows, cols) if state[0][k][l] == '#']
+        for k, l in cells:
+          probabilties[k][l] += n/len(cells)
+      if probabilties[i][j] > 0:
+        cell_prob.put([probabilties[i][j], (i, j)])
+        cellp.append([probabilties[i][j], (i, j)])
+  # print(cell_prob.get())
+  display_state([probabilties])
+  print(cellp)
+  p, (x, y) = cell_prob.get()
+  print(p)
+  return x, y
 
 
 def main():
@@ -86,29 +119,35 @@ def main():
   print(mines)
   print('-'*30)
   state = [[['#']*cols for _ in range(rows)], 0, 0] # exposed grid, number of exposed cells, flagged mines
+  probabilties = np.zeros([rows, cols])
+  explored = np.zeros([rows, cols], dtype=int)
   display_state(state)
   done = False
-  flag = False
+  first = True
+  # flag = False
   while not done:
     print('Click on ')
     try:
-      x = int(input('\trow: '))
-      y = int(input('\tcol: '))
+      # x = int(input('\trow: '))
+      # y = int(input('\tcol: '))
+      x, y = ai_player(state, probabilties, explored, first)
+      first = False
+      print('Computer chose ', x, y)
     except ValueError as _:
       print('Invalid input!')
       continue
-    done = play(x, y, state, board, flag)
+    done = play(x, y, state, board) #, flag)
     display_state(state)
     if (rows*cols - state[1]) == num_mines or state[2] == num_mines:
       print('Winner!')
       done = True
     elif not done:
-      flag = False
-      option = input('Continue (enter \'N\' to quit, \'F\' to flag)? ')
+      # flag = False
+      option = input('Continue (enter \'N\' to quit)? ') #', \'F\' to flag)'
       if option == 'N':
         break
-      if option == 'F':
-        flag = True
+      # if option == 'F':
+      #   flag = True
     else:
       print('Game Over!')
 
